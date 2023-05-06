a0e6e20a9688ff8e897cdb8107ab7608e5f0604a
Buibelle <131623341+Buibelle@users.noreply.github.com>
2023-05-06 15:02:38 -0700

    Add files via upload
    cleaning and merging the two dataframes

--- final project R.R
+++ final project R.R
@@ -2,6 +2,7 @@ library(stringr)
 library(dplyr)
 library(ggplot2)
 library(tidyr)
+library(tidyverse)
 
 emissions_df <- ghg_emissions
 pop_df <- apportionment
@@ -18,15 +19,26 @@ joined_df <- full_join(pop_df, emissions_df, by = "State")
 pop_1_df <- pop_df[pop_df$`Geography Type` == "State",]
 
 #remove unneeded years
-pop_2_df <- pop_1_df[pop_1_df$Year > 1990,]
+pop_2_df <- pop_1_df[pop_1_df$Year >= 1990,]
 
 #remove representitive data
 pop_3_df <- subset(pop_2_df, select = c("State", "Year", "Resident Population", "Percent Change in Resident Population", "Resident Population Density", "Resident Population Density Rank"))
 
 #cleaning emissions df
 emissions_1_df <- emissions_df[emissions_df$unit == "MtCO2e",]
-#emissions_2_df <- gather(emissions_1_df, key = "Year", value = "Value", 1990:2020)
-emissions_2_df <- unite(emissions_1_df, col = Year, cols(3:33)
 
-#add a column for gas level in combined df
-joined_1_df <- 
+# Transform the dataset
+emissions_2_df <- emissions_1_df %>%
+  gather(key = "Year", value = "units", `1990`:`2020`) %>%
+  select(`State`, Year, units)
+
+#change the name of the columns
+names(emissions_2_df)[3] <- "MtCO2e level"
+
+#change strings to numerics
+emissions_2_df$Year <- as.numeric(emissions_2_df$Year)
+
+#join the cleaned dataframes
+final_2_df <- full_join(emissions_2_df, pop_3_df, by = c("State", "Year"))
+
+final_df <- merge(emissions_2_df, pop_3_df, by = c("State", "Year"))
